25b95c1a3cb6f9e5faf2b810b170555417e680aa
Son Pham <son.ph@live.com>
2022-02-05 00:32:27 +1300

    added student dao functions: add new student, change password

--- src/main/java/constant/Constant.java
+++ src/main/java/constant/Constant.java
@@ -1,12 +1,14 @@
 package constant;
 
 public class Constant {
-	
+
 	public final static int BOOK_PER_PAGE = 10;
-	
+
 	public static int TOTAL_BOOKS;
-	
+
 	public static int TOTAL_PAGES;
-	
+
 	public static int CURRENT_PAGE;
+
+	public static String DEFAULT_STUDENT_PASSWORD = "codingStudent";
 }
--- src/main/java/dao/StudentDAO.java
+++ src/main/java/dao/StudentDAO.java
@@ -7,12 +7,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import constant.Constant;
 import constant.SecureHash;
 import db.DBUtil;
 import entity.Student;
 
 public class StudentDAO {
-	public Student getStudentByEmailAndPassword(String email, String password) throws SQLException {
+	public Student getStudentByEmailAndPassword1(String email, String password) throws SQLException {
 
 		Connection conn = null;
 		PreparedStatement ps = null;
@@ -71,7 +72,7 @@ public class StudentDAO {
 
 	}
 
-	public Student getStudentByEmailAndHashPassword(String email, String password) throws SQLException {
+	public Student getStudentByEmailAndPassword(String email, String password) throws SQLException {
 
 		Connection conn = null;
 		PreparedStatement ps = null;
@@ -117,6 +118,108 @@ public class StudentDAO {
 			}
 		}
 		return null;
+	}
+
+	public void changeStudentPassword(String email, String oldPassword, String newPassword) throws SQLException {
+
+		Connection conn = null;
+		PreparedStatement ps = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		Student student = null;
+
+		try {
+			conn = DBUtil.makeConnection();
+			String sql = "SELECT * FROM student WHERE email = ?";
+
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, email);
+			rs = ps.executeQuery();
+
+			if (rs.next()) { // rs.next is boolean 1/0
+				long id = rs.getLong("id");
+				String storedPassword = rs.getString("password");
+				String salt = rs.getString("salt");
+
+				boolean validation = SecureHash.validateSHA265Password(oldPassword, storedPassword, salt);
+
+				if (validation) {
+
+					String password = SecureHash.getPBKDF2Password(newPassword, salt);
+
+					sql = "UPDATE student SET password = ? where (id = ?)";
+					pstm = conn.prepareStatement(sql);
+					pstm.setString(1, password);
+					pstm.setLong(2, id);
+
+					pstm.executeUpdate();
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (ps != null) {
+				ps.close();
+			}
+			if (pstm != null) {
+				pstm.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+
+	}
+
+	public boolean addNewStudent(String firstName, String lastName, String email) throws SQLException {
+
+		Connection conn = null;
+		PreparedStatement ps = null;
+
+		ResultSet rs = null;
+
+		try {
+			conn = DBUtil.makeConnection();
+			String sql = "INSERT IGNORE INTO student (first_name, last_name, email, password, salt) VALUES (?,?,?, ?, ?)";
+
+			String salt = SecureHash.getSalt();
+			String defaultPassword = Constant.DEFAULT_STUDENT_PASSWORD;
+			String password = SecureHash.getPBKDF2Password(defaultPassword, salt);
+
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, firstName);
+			ps.setString(2, lastName);
+			ps.setString(3, email);
+			ps.setString(4, password);
+			ps.setString(4, salt);
+
+			int result = ps.executeUpdate();
+
+			if (result == 1) {
+
+				return true;
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (ps != null) {
+				ps.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+		return false;
+
 	}
 
 	public List<Student> getStudentList() throws SQLException {
